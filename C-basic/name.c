eec68f933ac9b79803c210b61fab3c1bb7f42044
Sandeep-Lodhi <90265623+Sandeep-Lodhi@users.noreply.github.com>
2021-12-18 21:43:57 +0530

    Add files via upload

---
+++ C-basic/name.c
@@ -0,0 +1,11 @@
+//1. Write a C program to print your name, date of birth. and mobile number. 
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+	printf("-------------------------------------->");
+	printf("\nName-> Sandeep Lodhi");
+	printf("\nDOB->01/01/2000");
+	printf("\nMobile No.->+91 2389398928");
+	
+}
